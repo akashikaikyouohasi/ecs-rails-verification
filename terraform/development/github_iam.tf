de2315d592bdff985f4b0d8750081783850c72aa
anikinthos <hayatata@live.jp>
2025-02-16 15:35:52 +0900

    feat: add IAM roles and policies for GitHub Actions Terraform apply workflow

--- terraform/development/github_iam.tf
+++ terraform/development/github_iam.tf
@@ -1,3 +1,4 @@
+### Plan用のIAM Role ### 
 data "aws_iam_policy_document" "github_oidc_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -72,4 +73,40 @@ resource "aws_iam_role_policy" "delete_lock_filepolicy" {
 resource "aws_iam_role_policy_attachment" "github_actions_terraform_plan_policy_attachment" {
   role       = aws_iam_role.github_actions_terraform_plan_role.name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+### Apply用のIAM Role ### 
+data "aws_iam_policy_document" "github_oidc_assume_role_policy_apply" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::206863353204:oidc-provider/token.actions.githubusercontent.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:akashikaikyouohasi/ecs-rails-verification:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_terraform_apply_role" {
+  name               = "GitHubActions_Terraform_AWS_terraform_apply"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role_policy_apply.json
+}
+
+# Administrator policyをroleにアタッチ
+resource "aws_iam_role_policy_attachment" "github_actions_terraform_apply_policy_attachment" {
+  role       = aws_iam_role.github_actions_terraform_apply_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
